989fd200f9208c3ac5464c3797da07e821096adb
Chrisvrsa <psychosisjr@gmail.com>
2025-07-29 21:06:53 -0400

    Added basic ability to add arguments to the script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,7 +8,12 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    if not len(sys.argv) > 1:
+        print("No arguments provided")
+        sys.exit(1)
+    else:
+        prompt = sys.argv[1]
+
 
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
     print(response.text)
