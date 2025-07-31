58c57b4715894e5ff87d9ec160ebc167d522fe2f
Chrisvrsa <psychosisjr@gmail.com>
2025-07-30 21:27:06 -0400

    Finished creating a stable argument system in main.py. Added a calculator program for testing the AI Agent in the future. Ran tests in calculator/tests to make sure its working properly

--- main.py
+++ main.py
@@ -2,24 +2,42 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    usage = "Usage: python main.py <User question to agent> [--verbose]"
 
-    if not len(sys.argv) > 1:
-        print("No arguments provided")
+    if len(sys.argv) == 1:
+        print("Error, no arguments given.")
+        print(usage)
         sys.exit(1)
-    else:
-        prompt = sys.argv[1]
 
+    # Capture all args except script name
+    user_args = sys.argv[1:]
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    # If verbose is in user arguments
+    # Will continue being trues here despite us removing verbose from user input
+    verbose = "--verbose" in user_args
+    if verbose:
+        while "--verbose" in user_args:
+            user_args.remove("--verbose")
+
+    # Now we join it
+    user_prompt = " ".join(user_args)
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    # This text is the response from the AI
     print(response.text)
-
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
