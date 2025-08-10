f873e68cb9d4df12505c7223838a679b352f3047
Chrisvrsa <psychosisjr@gmail.com>
2025-08-09 22:44:49 -0400

    Implemented call_function at the top of main() to call functions properly. Gave me a headache that never went away.

--- main.py
+++ main.py
@@ -3,13 +3,60 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-# schemas
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python import schema_run_python_file
-from functions.write_file import schema_write_file
 
-# Note to self. Always switch to .venv in the terminal before making changes
+# schemas and definitions
+from functions.get_files_info import *
+from functions.get_file_content import *
+from functions.run_python import *
+from functions.write_file import *
+
+# Note to self. Always switch to .venv in the terminal before making changes!
+
+# Extra function needed to call functions in main.py
+def call_function(function_call_part, verbose=False):
+    WORKING_DIRECTORY = "./calculator"
+
+    # Create a mapping of function names to their implementation
+    function_map = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file
+    }
+
+    #debug
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    
+    # Check if it exists, if not returns a types.Content with an error message
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"}
+                )
+            ],
+        )
+    args = dict(function_call_part.args)
+    args["working_directory"] = WORKING_DIRECTORY
+
+    function_result = function_map[function_name](**args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result}
+            )
+        ],
+    )
+    
 
 def main():
     current_dir = os.getcwd()
@@ -77,12 +124,21 @@ All paths you provide should be relative to the working directory. You do not ne
     # if the list is not empty
     if response.function_calls:
         list_of_responses = response.function_calls
+
         for function_call_part in list_of_responses:
-            function_call_name = function_call_part.name
-            function_call_args = function_call_part.args
-            print(f"Calling function: {function_call_name}({function_call_args})")
+           generated_content_result = call_function(function_call_part, verbose=verbose)
+           if not generated_content_result.parts[0].function_response.response:
+               raise ValueError("Something went critically wrong. No response from function call.")
+           else:
+               if verbose:
+                   print(f"-> {generated_content_result.parts[0].function_response.response}")
+           
     else:
         print(response.text)
 
 if __name__ == "__main__":
     main()
+
+
+
+    
